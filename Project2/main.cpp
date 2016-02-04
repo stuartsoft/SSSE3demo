844374abca91605c8dfac05752835f999a122a6a
Stuart Bowman <stuartsoft2012@gmail.com>
2016-02-04 02:45:34 -0500

    Added a bunch of comments

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -11,10 +11,29 @@
 using namespace std;
 
 //forward declarations
+
+//EmployeeSchedule(bool) is used to generate a company schedule, represented by a 16 byte array in the form of a __m128i type.
+//This function generates an array of 16 employee ids and 16 manager ids. It then generates a random 16 byte
+//schedule mask (b) to dictate the schema of the schedule for Employees and Managers. The schedule mask is
+//be used to generate shedules schemes following a pattern, as well as omit a certain number of employees from the resulting list.
+//So 1 particular mask could be used for making a regular company schedule, and another mask could be used for 
+//a holiday schedule. This would allow the schedule template to operate independently from the employee list
+//and make it very easy to update the schedule if the employee list changed, while still retaining the 
+//schedule scheme. Once the Employee schedule and manager shcedules are built, they are merged together using
+//the alignr instruction and returned by the function as a 16 byte array represented as a __m128i.
 __m128i employeeSchedule(bool RunOnHardware);
+
+//a test built to compare software vs hardware implementations of shuffling a byte array based on another byte array mask
 void shuffleTest();
+
+//a test built to compare software vs hardware implementations of concatinating, byte shifting, and trunkating back to a 128 bit output
 void alignrTest();
+
+//a test built to compare software vs hardware implementations of the _m_mulhrs_epi8 operation
 void mulhrsTest();
+
+//a test built to demonstrate a real world example of using the shuffle and alignr operations in tandem and 
+//the dramatic time savings when using the hardware implementation
 void employeeTest();
 
 __m128i mr1, mr2, mr3, mr4, mr5, mr6;
@@ -25,18 +44,21 @@ signed short ssb[8];
 
 int main (int argc, char* argv[]){
 
+	//Run shuffle calculation, comparing the runtime results for the software implementation and hardware instruction
 	shuffleTest();
 	cout<<endl;
 
-
+	//run alignr calculation, again comparing results
 	alignrTest();
 	cout<<endl;
 
+	//run mulhrs calculation, again comparing results
 	mulhrsTest();
 	cout<<endl;
 
 	cout<<"--------------------"<<endl;
 
+	//run the employee scheduler example, again comparing results
 	employeeTest();
 
 	system("pause");
@@ -45,7 +67,7 @@ int main (int argc, char* argv[]){
 void shuffleTest(){
 	StopWatch timer;
 	double elapsed;
-
+	//software implementation
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<16;i++){
 			b[i] = rand()%255;
@@ -57,6 +79,7 @@ void shuffleTest(){
 	}
 	cout << "Elapsed time for shuffle pseudo: \t" << elapsed << endl;
 
+	//hardware implementation
 	timer.reset();
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<16;i++){
@@ -74,6 +97,7 @@ void alignrTest(){
 	StopWatch timer;
 	double elapsed;
 
+	//software implementation
 	timer.reset();
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<16;i++){
@@ -86,6 +110,7 @@ void alignrTest(){
 	}
 	cout << "Elapsed time for alignr pseudo : \t" << elapsed << endl;
 
+	//hardware implementation
 	timer.reset();
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<16;i++){
@@ -103,6 +128,7 @@ void mulhrsTest(){
 	StopWatch timer;
 	double elapsed;
 
+	//software implementation
 	timer.reset();
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<8;i++){
@@ -115,6 +141,7 @@ void mulhrsTest(){
 	}
 	cout << "Elapsed time for mulhrs pseudo : \t" << elapsed << endl;
 
+	//hardware implementation
 	timer.reset();
 	for (int i = 0;i<ITERATIONS;i++){
 		for (int i = 0;i<8;i++){
@@ -130,6 +157,7 @@ void mulhrsTest(){
 
 void employeeTest(){
 	StopWatch timer;
+	//software implementation
 	timer.start();
 	for (int i = 0;i<ITERATIONS;i++){
 		employeeSchedule(false);
@@ -137,6 +165,7 @@ void employeeTest(){
 	double elapsed = timer.stop();
 	cout<< "Employee Schedule Pseudo: \t\t"<< elapsed<<endl;
 
+	//hardware implementation
 	timer.reset();
 	timer.start();
 	for (int i = 0;i<ITERATIONS;i++){
